75f6f0ca1b6088703a23f21df1b69c412c70275d
Hugo <hugo@42devs.cl>
2024-07-10 03:11:22 -0400

    feat: first version of user database queries

---
+++ server/models/user.ts
@@ -0,0 +1,52 @@
+import { PrismaClient } from '@prisma/client';
+import argon2, { argon2id } from 'argon2';
+
+const prisma = new PrismaClient({
+  omit: {
+    user: {
+      password: true,
+    }
+  }
+});
+
+// Uses secret as pepper
+const secret = Buffer.from(process.env.MAIN_SECRET || 'localSecret');
+
+export const getUserByUsername = async (username: string) => {
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        username: username,
+      },
+    });
+    return user;
+  } catch (e) {
+    throw new Error('User Not Found');
+  }
+};
+
+export const createUser = async (username: string, password: string) => {
+  // Not using custom salt since is included in argon2
+  const hash = await argon2.hash(password, { secret });
+  const createdUser = await prisma.user.create({
+    data: {
+      username,
+      password: hash,
+    }
+  })
+  return createdUser;
+};
+
+export const validateUserPassword = async (username: string, password: string) => {
+  const user = await prisma.user.findUnique({
+    where: { username },
+    select: {
+      username: true,
+      password: true,
+    }
+  });
+  if (!user) throw new Error('User Not Found');
+  const result = await argon2.verify(user.password, password, { secret });
+  if (!result) throw new Error('Password Invalid');
+  return result;
+};
