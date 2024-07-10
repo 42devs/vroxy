2f1d855929f872d673265e11e32d12d300c95f3d
Hugo <hugo@42devs.cl>
2024-07-10 00:44:17 -0400

    feat: Added default font

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,9 +3,15 @@ import Aura from '@primevue/themes/aura';
 export default defineNuxtConfig({
   devtools: { enabled: true },
 
-  modules: [
-    '@primevue/nuxt-module',
-  ],
+  css: ['~/assets/css/main.css'],
+
+  modules: ['@primevue/nuxt-module', "@nuxtjs/google-fonts"],
+
+  googleFonts: {
+    families: {
+      Roboto: true,
+    }
+  },
 
   primevue: {
     /* Configuration */
