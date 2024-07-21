31cf6a05367fc1550c7b7ff4a4f7384feb7fff76
Hugo <hugo@42devs.cl>
2024-07-21 05:53:19 -0400

    feat: base trpc procedure added to nuxt

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,22 @@
 <template>
-  <Button label="Click Me!"></Button>
+  <div>
+    <Button
+      label="Click Me!"
+      @click="updateResult"
+    />
+    <pre>{{ result }}</pre>
+  </div>
 </template>
+
+<script setup lang="ts">
+const { $client } = useNuxtApp();
+
+const text = ref('hugo');
+
+const result = ref<unknown>({});
+
+const updateResult = async () => {
+  const queryResult = await $client.hello.query({ name: text.value });
+  result.value = queryResult;
+};
+</script>
